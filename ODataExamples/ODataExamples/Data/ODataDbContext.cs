16b7b7733fd8c25df3d5f8a7452439edd3ab0143
anil.chhetri <anil.chhetri@cotiviti.com>
2022-01-18 15:45:05 +0545

    Added model and DBContext and perform initial migrations

---
+++ ODataExamples/ODataExamples/Data/ODataDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataExamples.Data
+{
+    public class ODataDbContext : DbContext
+    {
+        public ODataDbContext(DbContextOptions<ODataDbContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<Customer> Customer { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
--- ODataExamples/ODataExamples/Startup.cs
+++ ODataExamples/ODataExamples/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ODataExamples.Data;
 
 namespace ODataExamples
 {
@@ -25,6 +20,8 @@ namespace ODataExamples
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<ODataDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("default")));
 
             services.AddControllers();
         }
