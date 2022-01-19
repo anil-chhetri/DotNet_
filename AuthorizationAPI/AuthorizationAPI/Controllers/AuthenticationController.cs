16184645078bfb855cc3d27a94880ef31bed8aec
anil.chhetri <anil.chhetri@cotiviti.com>
2022-01-19 12:02:26 +0545

    TODO Later

---
+++ AuthorizationAPI/AuthorizationAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,56 @@
+﻿using AuthorizationAPI.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public partial class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManger;
+        private readonly IConfiguration _config;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManger = roleManager;
+            _config = configuration;
+        }
+
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel register)
+        {
+            var user = await _userManager.FindByNameAsync(register.UserName);
+            if(user != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response {Status="Error", Messages="User Already Exists." });
+            }
+
+            ApplicationUser applicationUser = new ApplicationUser()
+            {
+                UserName = register.UserName,
+                Email = register.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, register.Password);
+
+            if(!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "error", Messages = "something went wrong." });
+            }
+
+            return Ok(new Response { Status = "success", Messages = "user created successfully." });
+        }
+    }
+}
